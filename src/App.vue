532bce1abc912d957a555769cf2266ed04cd6889
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-07 17:22:41 +0100

    Added fontawesome + bootstrap + added SectionMenu

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,26 @@
   <div class="container">
     <Title />
     <Jumbotron />
+    <SectionMenu />
   </div>
 </template>
 
 <script>
 import Title from "./components/Title.vue";
 import Jumbotron from "./components/Jumbotron.vue";
-
+import SectionMenu from "./components/SectionMenu.vue";
 export default {
   name: "App",
   components: {
     Title,
     Jumbotron,
+    SectionMenu,
   },
 };
 </script>
 
 <style>
-.container{
+.container {
   background-color: cadetblue;
 }
 </style>
---
+++ src/components/SectionMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="SectionMenu">
+    <h2>Come mi sento oggi?</h2>
+    <div>
+      <ul>
+        <li>Super Abbacchio <i class="fas fa-smile-wink"></i></li>
+        <li>Mezz'Abbacchio <i class="fas fa-meh-rolling-eyes"></i></li>
+        <li>Abbacchiato <i class="fas fa-sad-cry"></i></li>
+        <li>Scottadito <i class="fas fa-turkey"></i></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SectionMenu",
+};
+</script>
+
+<style>
+.SectionMenu {
+  text-align: center;
+}
+h2 {
+  margin-top: 10px;
+}
+ul {
+  list-style-type: none;
+}
+</style>
